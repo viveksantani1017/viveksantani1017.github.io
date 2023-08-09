96c33c4bf32e4dafa8f06dd620d0f56ba6af220b
viveksantani1017 <viveksantani0@gmail.com>
2023-08-09 19:08:00 +0530

    Added Education

---
+++ src/components/Education.jsx
@@ -0,0 +1,65 @@
+const education = [
+  {
+    name: "Institute Of Technology, Nirma University.Ahmedabad",
+    link: "https://nirmauni.ac.in/",
+    date: "2022 / 2024",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/en/8/83/Nirma_University_Logo.png",
+    course: "M.C.A ( Master of Computer Application)",
+    cgpa: 8.45,
+  },
+  {
+    name: "Smt. C.Z.M Gosrani B.C.A College.Jamnagar",
+    link: "https://czmgbca.oshwaleducationtrust.org/",
+    date: "2019 / 2022",
+    imageUrl: "https://czmgbca.oshwaleducationtrust.org/img/logo.png",
+    course: "B.C.A ( Bachelor of Computer Application)",
+    cgpa: 8.86,
+  },
+];
+
+export default function Education() {
+  return (
+    <div className="">
+      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 mb-20">
+      <h2 className="text-2xl font-bold text-gray-900 mb-12">EDUCATION</h2>
+        <ul role="list" className="divide-y divide-gray-100">
+          {education.map((item) => (
+            <li key={item.email} className="flex justify-between gap-x-6 py-5">
+              <div className="flex min-w-0 gap-x-4">
+                <img
+                  className="h-12 w-12 flex-none rounded-full bg-gray-50"
+                  src={item.imageUrl}
+                  alt=""
+                />
+                <div className="min-w-0 flex-auto">
+                  <a
+                    className="text-md font-semibold leading-6 text-gray-900"
+                    target="blank"
+                    href={item.link}
+                  >
+                    {item.name}
+                  </a>
+                  <p
+                    className="mt-1 truncate text-sm leading-5 text-gray-500"
+                    target="_blank"
+                  >
+                    Course: {item.course}
+                  </p>
+                </div>
+              </div>
+              <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
+                <p className="text-md leading-6 text-gray-900">
+                  CGPA: {item.cgpa}
+                </p>
+                <p className="mt-1 text-sm leading-5 text-gray-500">
+                  {item.date}
+                    </p>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,6 +13,8 @@ import mysqlLogo from "../assets/mysqlLogo.png";
 import mongoLogo from "../assets/mongoLogo.png";
 import chakraLogo from "../assets/chakraLogo.png";
 import bootstrapLogo from "../assets/bootstrapLogo.png";
+import Project from "../components/Project";
+import Education from "../components/Education";
 
 const navigation = [
   { name: "Experience", href: "#" },
@@ -122,7 +124,7 @@ export default function Home() {
       </header>
 
       <div className="relative isolate px-6 pt-14 lg:px-8">
-        <div
+        {/* <div
           className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
           aria-hidden="true"
         >
@@ -133,7 +135,7 @@ export default function Home() {
                 "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
             }}
           />
-        </div>
+        </div> */}
         <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
           <div>
             <h1 className="text-2xl font-bold text-left tracking-tight text-gray-600 sm:text">
@@ -168,7 +170,7 @@ export default function Home() {
             </div>
           </div>
         </div>
-        <div
+        {/* <div
           className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
           aria-hidden="true"
         >
@@ -179,9 +181,9 @@ export default function Home() {
                 "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
             }}
           />
-        </div>
+        </div> */}
 
-        <div class="">
+        <div>
           <div class="mx-auto max-w-7xl px-6 lg:px-8">
             <h2 class="text-center text-4xl font-bold leading-8 text-gray-900">
               Techstacks
@@ -231,8 +233,14 @@ export default function Home() {
               />
             </div>
           </div>
+      <Project/>
+      <Education/>
         </div>
-      </div>
+              </div>
+
+
+      
+
     </div>
   );
 }
